5fb9a4a00050d7325c30c718a3f6c9abc9854581
Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>
2022-06-09 12:21:31 +0000

    feat(cctxviz): workload test
    Signed-off-by: Rafael Belchior <rafael.belchior@tecnico.ulisboa.pt>

---
+++ packages/cactus-plugin-cc-tx-visualization/src/main/python/run-generate-cross-chain-model.py
@@ -0,0 +1,54 @@
+import subprocess
+import time
+import sys
+import os
+
+# Input1: name of test CSV file that generates the model
+# Input2: number of test runs
+# Output: output files corresponding to number of runs on packages/cactus-plugin-cc-tx-visualization/src/main/test-results  
+
+def main():
+    start = time.time()
+    file = open(SAVE_STRING, "wb")
+
+    process = subprocess.Popen(FULL_COMMAND.split(), cwd="/home/rbelchior/blockchain-integration-framework", stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    print("error: \n {}", error)
+    #outputText = sys.stdout.buffer.write(bytes(output,"utf-8"))
+    file.write(output)
+    file.close()
+
+    end = time.time()
+
+    print("\n==========")
+    print("\nPartial Running time: {:01f}\n".format(end-start))
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    testName = args[0]
+    numberTests = args[1]
+    cumulativeTimeStart = time.time()
+    TEST_EXTENSION = ".test.ts"
+    OUTPUT_DIR = "packages/cactus-plugin-cc-tx-visualization/src/main/test-results/"
+    TEST_DIR = "packages/cactus-plugin-cc-tx-visualization/src/test/typescript/integration/"
+    # directory is given as input to subprocess
+    VS_CODE_PARTIAL_SCRIPT = "npx tap --ts --timeout=600 "
+    TARGET =  VS_CODE_PARTIAL_SCRIPT + TEST_DIR + testName + TEST_EXTENSION
+    FULL_COMMAND = TARGET
+    print("Running: ", FULL_COMMAND)
+
+    runs = int(numberTests)
+    while runs > 0:
+        SAVE_STRING =  os.path.join("../", "test-results/") + testName + "-" + str(runs) + ".out"
+        print("Saving out in:", SAVE_STRING)
+        print("Iteration %\n ", runs)
+        main()
+        runs -= 1
+    cumulativeTimeEnd = time.time()
+    print("\n==========")
+    print("\nTotal number of tests done:",numberTests )
+    print("\n==========")
+    print("\nType of tests done:",testName )
+    print("\n==========")
+    print("\nTotal Running time: {:01f}\n".format(cumulativeTimeEnd-cumulativeTimeStart))
